a70f14b32cde42131227aa1d7e2957e4ff8935f5
Alexandre Hurstel <alexandre.hurstel@mailoo.org>
2020-05-28 10:19:50 +0200

    removing unnecessay commands, tweaked '!kart help' display

--- js/commands/cmd_kart.js
+++ js/commands/cmd_kart.js
@@ -1,16 +1,4 @@
 
-//CLEARANCE LEVEL:
-// A particular member can have 3 different clearance level:
-//  - CLEARANCE_LEVEL.NONE = 0 = 0b000 - no clearance level
-//  - CLEARANCE_LEVEL.ADMIN_ROLE = 0b010 = 2  - member is recognized admin
-//  - CLEARANCE_LEVEL.MASTER_ID = 0b100 = 4  - member is the bot's master
-// A fourth level exists, used to contextualized a message:
-//  - CLEARANCE_LEVEL.CONTROL_CHANNEL = 1 = 0b001 - posted in context of a 'control channel'
-// Ofc, these level are stackable:
-//  A clearance level of 7:
-//    7 = 1+2+4 = 0b111 = CONTROL_CHANNEL+ADMIN_ROLE+MASTER_ID
-//      is obtained when the master, who here is also an admin, posted a message in a
-//      control channel
 const CLEARANCE_LEVEL= require('../defines').CLEARANCE_LEVEL;
 
 const child_process= require("child_process");
@@ -80,57 +68,12 @@ function _getPassword(){
     return stdout;
 };
 
-//when the module is loaded, during the bot's launch,
-//this function is called.
-//  utils is an object provided by the bot, constructed as follow:
-//    utils= {
-//     settings: {
-//        set: function (guild, field, value) ,
-//        get: function (guild, field),
-//        remove: function (guild, field)
-//     },
-//     getMemberClearanceLevel: function(member),
-//     getBotClient: function(),
-//     cache_message_management:{
-//        keepTrackOf: function (msg),
-//        untrack: function (msg),
-//        isTracked: function (msg),
-//     },
-//     getMasterID: function()
-//    }
-//  Where:
-//    'utils.settings.get(guild, field)' is a function you need to call when
-//      you whant to access an saved object (json formatable) in this module's
-//      persitent data.
-//      Example: onlineRole= utils.settings.get(guild, 'online-role');
-//    'utils.settings.set(guild, field, value)' is a function you need to call when
-//      you want to save data (json formatable object) in this module's persistent
-//      data.
-//      Example: utils.settings.set(guild, 'number-of-cats', 5);
-//    'utils.remove(guild, field)' is a function you need to call when you want to remove
-//       a previously saved object of this module's persitent data.
-//       Example: utils.remove(guild, 'online-role');
-//    'utils.getMemberClearanceLevel(member)' is a function you need to call in order to know
-//       the 'clearance level' of a particular guild member. (see below for clearance level)
-//    'utils.getBotClient' return the discord bot client.
-//    'utils.cache_message_management.keepTrackOf(msg)' is a function to want to call when you
-//      want to make sure a message is kept in the cache indefinetely. This is usefull, for
-//      example, when you are tracking reaction on a given message indefinetly, keep it from
-//      eventually being thrown out of the cache, and not recieving any notifications about this
-//      message anymore.
-//    'utils.cache_message_management.untrack(msg)' is a function to call when you no longer need
-//      for a particular message to being kept in cache.
-//    'utils.getMasterID' returns the bot's master's user ID.
+
 function cmd_init(utils){
 
 }
 
 
-
-//this function is called, during bot's launch (after 'cmd_init'),
-//once per guild the bot is part of.
-//It is the opprtunity, for example, to verify the data's integrity
-//and coherence with the current state of the guild…
 async function cmd_init_per_guild(utils, guild){
     var servOwner= utils.settings.get(guild, "serv_owner");
     var m_owner= undefined;
@@ -153,21 +96,6 @@ async function cmd_init_per_guild(utils, guild){
 }
 
 
-
-//this function is called when a command registered by this module
-//(see end of this file) has been called in a guild.
-// 'clearanceLvl' is the clearance level in wich this command has been posted
-// 'cmdObj' is an object provided by the bot, constructed as follow:
-//   cmdObj={
-//      'command',
-//      'args',
-//      'msg_obj'
-//   }
-//  Where:
-//   'cmdObj.command' is the string that constitutes the command called.
-//   'cmdObj.args' is an array containing each remaining words of the command
-//   'cmdObj.msg_obj' is the discord message object associated with the command
-//     (see https://discord.js.org/#/docs/main/stable/class/Message)
 async function cmd_main(cmdObj, clearanceLvl, utils){
     let message= cmdObj.msg_obj;
     let args= cmdObj.args;
@@ -401,22 +329,20 @@ async function cmd_main(cmdObj, clearanceLvl, utils){
 }
 
 
-
-//this function is called when a 'help' command has been called in a
-//guild, regarding one of the commands registered by this module.
 function cmd_help(cmdObj, clearanceLvl){
     cmdObj.msg_obj.author.send(
         "⚠ **_IMPORTANT:_** SRB2Kart server is exclusively for Strasbourg Smasher's usage. Please **always** shutdown the server (`!kart stop`) once the playing session is over\n\n"+
         `__**kart** command___:\n\n`+
-        "**this command is only usable in the designated \"srb2kart channel\"!**\n\n"+
         ((clearanceLvl<CLEARANCE_LEVEL.ADMIN)? "": ("**Admins only (usable in other channels):**\n\n"+
             "\t`!kart channel #channelmention`\n\n"+
             "\tset which channel gets to be the *designated srb2kart channel*\n\n"+
             "\t`!kart channel clear`\n\n"+
             "\tunset the *designated srb2kart channel*\n\n"+
             "\t`!kart channel which`\n\n"+
-            "\ttells which channel is set as the *designated srb2kart channel*\n\n"
+            "\ttells which channel is set as the *designated srb2kart channel*\n\n"+
+            "**All users commands:**\n"
         )) +
+        "**Following commands are only usable in the designated \"srb2kart channel\"!**\n\n"+
         "\t`!kart start`\n\n"+
         "\tTry to start the SRB2Kart server.\n\tIf success, the server password is send via private message, and send is considered as the *designated admin* of the server.\n\n"+
         "\t`!kart stop`\n\n"+
@@ -438,9 +364,6 @@ function cmd_help(cmdObj, clearanceLvl){
 }
 
 
-
-//this function is called when an event has been recieved by the bot's client.
-//See https://discord.js.org/#/docs/main/stable/class/Client for the event list).
 function cmd_event(eventName, utils){
     if(eventName==="channelDelete"){
         var channel= arguments[2];
@@ -481,15 +404,8 @@ function cmd_event(eventName, utils){
 }
 
 
-
-//this function is called when the bot leaves a guild
 function cmd_guild_clear(guild){}
 
 
-
-//the module then needs to register these function for export
-//  set 'module.exports.name' to a the name of a command this module wants to register.
-//  it can registers several commands by providing an array of strings.
 module.exports.name= ['kart'];
-//  all the functions previously presented needs to be register is a grouped object, as the following:
 module.exports.command= {init: cmd_init, init_per_guild: cmd_init_per_guild, main: cmd_main, help: cmd_help, event: cmd_event, clear_guild: cmd_guild_clear};
