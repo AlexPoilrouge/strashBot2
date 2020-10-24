ece4a083de8b435136fb329d06746a1e4a1d5a32
AlexPoilrouge <alexandre.hurstel@mailoo.org>
2020-10-24 14:51:25 +0200

    completing !help top8

--- js/commands/cmd_top8gen.js
+++ js/commands/cmd_top8gen.js
@@ -870,9 +870,10 @@ function cmd_help(cmdObj, clearanceLvl){
         `---\n**Following commands only availabe to members of designated group** (see \`!${prt_cmd} group\`):\n\n`+
         `\t\`!${prt_cmd} <template> [smashggUrl] [options…]\`\n\n`+
         `\tgenerates top8 from a given template (get available templates list with \`!${prt_cmd} template\`)\n`+
+        `\tFor character roster informations about players, the data is lookup in the database of the guild (see \`!player\` & \`!roster\` commands)\n`+
         `\tIf a smashgg Url is provided, then top8 data will be fetch from this smash.gg tournament.\n\n`+
         `\t⚠️ This assumes that the tournament is completed, and that the provided smash.gg Url points to a '*Singles' event.\n`+
-        `\t\t__Example:__ \`!${prt_cmd}\` template <https://smash.gg/tournament/scarlet-arena-4/event/singles> ?title="4th edition"`+
+        `\t\t__Example:__ \`!${prt_cmd} template https://smash.gg/tournament/scarlet-arena-4/event/singles ?title="4th edition"\n\n`+
         `\t\`!${prt_cmd} test <template> [smashggUrl] [options…]\`\n\n`+
         `\tThe goal of the commands is to test out parameters and options to ensure their validity before making an actual`+
         `call to the \`!top8\` command.\n` +
