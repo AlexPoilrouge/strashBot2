0250f8a4098521bcfaddb56406148b86be3bf4d0
AlexPoilrouge <alexandre.hurstel@mailoo.org>
2021-06-19 10:04:14 +0200

    preparing g_calendar read module

--- js/utils.js
+++ js/utils.js
@@ -314,6 +314,45 @@ function sleep(ms=1000){
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+function DateFromTimeZone(dateString, timezone="Europe/Paris"){
+    var baseDate= new Date(dateString)
+    var tzPoVDate= new Date(baseDate.toLocaleString('en-US', {
+        timeZone: timezone
+    }))
+
+    var diff= baseDate.getTime() - tzPoVDate.getTime()
+
+    return new Date(baseDate.getTime() + diff)
+}
+
+
+
+function identifyEmoji(str, utils){
+    let simpleEmojiRegex= /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+    let customEmojiRegex= /^<\:([a-zA-Z\-_0-9]+)\:([0-9]{18})>$/;
+
+    var _tmp= undefined;
+    var emojiType= undefined;
+    hereLog(`[identifyEmoji] ${str}`)
+    try{
+        emojiType= ( Boolean(str) )? (
+                Boolean(str.match(simpleEmojiRegex))? {type: "SIMPLE", emoji: str, text: str} :
+                    ( Boolean(str) && Boolean(_tmp=str.match(customEmojiRegex))? 
+                            {   type: "CUSTOM",
+                                emoji: utils.getBotClient().emojis.cache.get(_tmp[2]),
+                                text: str
+                            }
+                        : undefined) 
+            ) : undefined;
+    }
+    catch(err){
+        hereLog(`[identifyEmoji] error: ${err.message}`)
+        emojiType= undefined;
+    }
+
+    return emojiType;
+}
+
 module.exports.JSONCheck= JSONCheck;
 module.exports.commandDecompose= commandDecompose;
 module.exports.commandNameFromFilePath= commandNameFromFilePath;
@@ -321,3 +360,5 @@ module.exports.commandArgsOptionsExtract= commandArgsOptionsExtract;
 module.exports.splitString= splitString;
 module.exports.DataBaseManager= DataBaseManager;
 module.exports.sleep= sleep;
+module.exports.DateFromTimeZone= DateFromTimeZone;
+module.exports.identifyEmoji= identifyEmoji;
